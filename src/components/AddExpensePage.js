3589e8c34db398306cbe0467ac6526bbc124908e
Harry <harryle.au@gmail.com>
2018-06-16 01:07:45 +1000

    add test cases for AddExpensePage component, refactor AddExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from './../actions/expenses';
+import { readSync } from 'fs';
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  };
 
-export default connect()(AddExpensePage);
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+// just like mapStateToProps, it turns dispatch actions into props, so in the component we don't need to use props.dispatch(addExpense(expense)) anymore.
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (expense) => dispatch(addExpense(expense))
+});
+
+// first arg is mapStateToProps, we don't need it in this component, so set it to be undefined
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
