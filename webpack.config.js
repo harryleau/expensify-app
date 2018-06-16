82077d90864c5097f3c37d6f3cf96ffa52c2b2cc
Harry <harryle.au@gmail.com>
2018-06-16 16:17:31 +1000

    change webpack config for extracting css file, add styles.css file to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const ExtractTextPlugin= require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
   
   return {
     entry: './src/app.js',
@@ -17,14 +19,27 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       port: 3000,
@@ -37,3 +52,5 @@ module.exports = (env) => {
 };
 
 // source-map takes long time to build but it's much lighter => use for production.
+// cheap-module-eval-source-map doesn't work well with css => use inline-source-map
+// by default, css-loader and sass-loader sourceMap prop is false => switch it to true => easy to track css error.
