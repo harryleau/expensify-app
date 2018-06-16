570edcb11b9751386376066df482b42a1e2dc7bf
Harry <harryle.au@gmail.com>
2018-06-16 15:51:14 +1000

    Fix error in ExpenseListFilters, add webpack config for production

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -75,4 +75,4 @@ const mapDispatchToProps = dispatch => ({
   setEndDate: (endDate) => dispatch(setEndDate(endDate))
 });
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: 3000,
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      port: 3000,
+      historyApiFallback: true
+    },
+    performance: {
+      hints: false
+    }
+  };
 };
+
+// source-map takes long time to build but it's much lighter => use for production.
